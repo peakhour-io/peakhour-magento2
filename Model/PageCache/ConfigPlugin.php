f5e5bfec10c5697c67c36d43721709598aff2da4
peakhour-io <50435442+peakhour-io@users.noreply.github.com>
2023-10-31 11:13:17 +1100

    Update ConfigPlugin.php
    Add method to convert legacy cache type from string to new int value.

--- Model/PageCache/ConfigPlugin.php
+++ Model/PageCache/ConfigPlugin.php
@@ -6,12 +6,41 @@ use \Magento\PageCache\Model\Config;
 
 class ConfigPlugin
 {
+    protected $_scopeConfig;
+
+    /**
+     * ConfigPlugin constructor.
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        $this->_scopeConfig = $scopeConfig;
+    }
+    
     public function afterGetType(Config $config, $result)
     {
         if (!($config instanceof \Peakhour\Cdn\Model\Config)) {
             if ($result == \Peakhour\Cdn\Model\Config::PEAKHOUR) {
                 return Config::VARNISH;
             }
+        }
+        return $result;
+    }
+
+    /**
+     * Change return value from string to int, 'peakhour' is old value
+     *
+     * @param Config $config
+     * @param callable $proceed
+     * @return string|int
+     */
+    public function aroundGetType(Config $config, callable $proceed)
+    {
+        if ($this->_scopeConfig->getValue(Config::XML_PAGECACHE_TYPE) === 'peakhour') {
+            $result = \Peakhour\Cdn\Model\Config::PEAKHOUR;
+        } else {
+            $result = (int)$proceed();
         }
         return $result;
     }
